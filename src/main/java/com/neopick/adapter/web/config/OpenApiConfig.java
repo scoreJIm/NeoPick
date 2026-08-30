04d3d1f2ae91e7c673729fed13abf0e39345fe4f
JimWei <vvlovqq@gmail.com>
2026-08-30 14:09:38 +0800

    docs: fix OpenAPI contact/server

--- src/main/java/com/neopick/adapter/web/config/OpenApiConfig.java
+++ src/main/java/com/neopick/adapter/web/config/OpenApiConfig.java
@@ -27,14 +27,14 @@ public class OpenApiConfig {
                         .version("1.0")
                         .description("Guitar lesson booking marketplace")
                         .contact(new Contact()
-                                .name("NEOPick Team")
-                                .email("dev@neopick.com"))
+                                .name("Wei Wei")
+                                .email("vvlovqq@gmail.com"))
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("https://www.apache.org/licenses/LICENSE-2.0")))
                 .servers(List.of(
                         new Server().url("http://localhost:8080").description("Local Development"),
-                        new Server().url("https://api.neopick.com").description("Production")
+                        new Server().url("https://neo.jimmyweidev.com").description("Production")
                 ))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .components(new Components()
